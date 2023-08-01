67534b1bc16fc13be93ded4a59a22ef111dc6f3a
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-08-01 15:13:07 +0600

    some changes

--- src/componenets/useInstructor.jsx
+++ src/componenets/useInstructor.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
-import axios from "axios";
+// import axios from "axios";
 
 const useInstructor = () => {
   const { user } = useAuth();
--- src/componenets/usePayment.jsx
+++ src/componenets/usePayment.jsx
@@ -1,16 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
 
 const usePayment = () => {
+  const { user } = useAuth();
   const [axiosSecure] = useAxiosSecure();
   const {
     data: payment = [],
     refetch,
     isLoading,
   } = useQuery({
-    queryKey: ["payments"],
+    queryKey: ["payments", user?.email],
     queryFn: async () => {
-      const res = await axiosSecure.get("/payments");
+      const res = await axiosSecure.get(
+        `http://localhost:5000/payments/${user?.email}`
+      );
       return res.data;
     },
   });
